476524f8356f0e8d664205817a7d48a4f7dd4170
unknown <emirhankabaci7@gmail.com>
2024-10-31 00:53:54 +0100

    Initial commit

--- public/js/VideogameCalendar-js/main.js
+++ public/js/VideogameCalendar-js/main.js
@@ -25,44 +25,28 @@ async function handleCreatePost() {
     }
 }
 
-function handleCredentialResponse(response) {
-    const id_token = response.credential; // Den ID-Token aus der Antwort erhalten
-    console.log("ID Token:", id_token);
-
-    // Token an den Server senden
-    sendTokenToServer(id_token);
-}
-
-// Funktion, um den Token an den Server zu senden
-async function sendTokenToServer(id_token) {
-    try {
-        const response = await fetch('https://videogamecalendarmbackend.apps.01.cf.eu01.stackit.cloud/api/auth/google', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ id_token }) // ID-Token im JSON-Format senden
-        });
-
-        if (!response.ok) {
-            throw new Error('Fehler beim Senden des Tokens: ' + response.statusText);
-        }
-
-        const data = await response.json();
-        console.log('Serverantwort:', data);
-
-        // Optional: Hier kannst du die nächste Aktion durchführen, z.B. Weiterleitung
-        if (data.redirect) {
-            window.location.href = data.redirect;
-        }
-    } catch (error) {
-        console.error('Fehler beim Senden des Tokens:', error);
+// Google Login Callback
+function onLoad() {
+    if (!window.gapi) {
+        console.error('Google API nicht verfügbar');
+        return;
     }
+
+    gapi.load('auth2', () => {
+        gapi.auth2.init({
+            client_id: '495089736315-cctdkib2v9sav9t0k7qv1mvestilf443.apps.googleusercontent.com'
+        }).then(() => {
+            const googleLoginButton = document.querySelector('.g_id_signin');
+            if (googleLoginButton) {
+                googleLoginButton.onclick = handleGoogleSignIn; // Event Listener vereinfachen
+            } else {
+                console.error('Google Login Button nicht gefunden.');
+            }
+        }).catch(error => {
+            console.error('Fehler bei der Authentifizierung:', error);
+        });
+    });
 }
-
-
-
-
 
 // Funktion zum Einloggen mit Google
 function handleGoogleSignIn() {
@@ -76,8 +60,6 @@ function handleGoogleSignIn() {
 async function onSignIn(googleUser) {
     const id_token = googleUser.getAuthResponse().id_token;
     console.log("ID Token:", id_token);
-
-    // Token an den Server senden
     await sendTokenToServer(id_token);
 }
 
