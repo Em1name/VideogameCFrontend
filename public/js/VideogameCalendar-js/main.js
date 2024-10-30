f3d14a5887277deab1ccf835ca9cd2df3da4d5a9
unknown <emirhankabaci7@gmail.com>
2024-10-30 13:01:47 +0100

    Änderung

--- public/js/VideogameCalendar-js/main.js
+++ public/js/VideogameCalendar-js/main.js
@@ -1,4 +1,4 @@
-import {setUsername, createPost, fetchPosts, checkAuthStatus, uploadProfilePicture} from '../VideogameCalender-js/api.js';
+import {setUsername, createPost, fetchPosts, checkAuthStatus, uploadProfilePicture} from './api.js';
 
 // Funktion zum Erstellen eines neuen Beitrags
 async function handleCreatePost() {
